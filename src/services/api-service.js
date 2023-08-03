2a1e93d90b4afc0ca65c04ec07254434d40b8452
StaTov <tovchfamily@gmail.com>
2023-08-03 11:59:37 +0400

    change auth header

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -77,6 +77,7 @@ export const uploadAll = async (files) => {
                 headers: {
                     'content-type': f.type,
                     'content-length': `${f.size}`,
+                    'Authorization': `OAuth ${token}`,
                 },
                 body: f,
 
