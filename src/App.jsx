9ac7fa33ed81687c2e65f5baac7852cb2dbbe446
StaTov <tovchfamily@gmail.com>
2023-08-07 01:58:22 +0400

    add setToken ap

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
   const [token, setToken] = useState(null);
 
   if (localStorage.getItem('access_token')) {
-    setToken(localStorage.getItem('access_token'))
+    console.log(localStorage.getItem('access_token'))
   }
-  
+
   useEffect(() => {
     
     if (!token) {
